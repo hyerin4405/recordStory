4961f19d49fe35a27ca3e044b6bb36056f8c9d2a
hyerin4405 <134987093+hyerin4405@users.noreply.github.com>
2024-03-12 13:39:15 +0900

    -SHR

---
+++ src/main/java/com/home/recordstory/RecordStoryApplication.java
@@ -0,0 +1,13 @@
+package com.home.recordstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/recordstory/RecordStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.recordstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
